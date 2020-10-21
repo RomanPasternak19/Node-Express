8b05915771ba4ead68b78915423629f195576272
Roman Pasternak <38011782+RomanPasternak@users.noreply.github.com>
2020-10-21 23:34:14 +0300

    add basket

--- models/card.js
+++ models/card.js
@@ -4,16 +4,39 @@ const { resolve } = require('path')
 const { rejects } = require('assert')
 
 const p = path.join(
-    require.main.filename,
+    path.dirname(require.main.filename),
     'data',
-    'card,json'
+    'card.json'
 )
 
 class Card{
     static async add(course){
         const card = await Card.fetch()
 
-        const idx = card.courses.findIndex(c => c.id ===course.id)
+        const idx = card.courses.findIndex(c => c.id === course.id)
+        const candidate = card.courses[idx]
+
+        if (candidate) {
+            //have course
+            candidate.count++
+            card.courses[idx] = candidate
+        } else {
+            //need add course
+            course.count = 1
+            card.courses.push(course)
+        }
+
+        card.price += +course.price
+
+        return new Promise((resolve, rejects) =>{
+            fs.writeFile(p, JSON.stringify(card), err =>{
+                if(err){
+                    rejects(err)
+                } else {
+                    resolve()
+                }
+            })
+        })
     }
     
     static async fetch(){
