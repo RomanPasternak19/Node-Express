090fe0400a14fdbc82919be23218d156b86e1426
Roman Pasternak <38011782+RomanPasternak@users.noreply.github.com>
2020-10-21 22:06:51 +0300

    add card,js

--- models/card.js
+++ models/card.js
@@ -1,10 +1,29 @@
+const path = require('path')
+const fs = require('fs')
+const { resolve } = require('path')
+const { rejects } = require('assert')
+
+const p = path.join(
+    require.main.filename,
+    'data',
+    'card,json'
+)
+
 class Card{
     add(){
 
     }
     
-    fetch(){
-
+    static async fetch(){
+        return new Promise((resolve,rejects) =>{
+            fs.readFile(p, 'utf-8', (err, content) =>{
+                if(err){
+                    rejects(err)
+                } else {
+                    resolve(JSON.parse(content))
+                }
+            })
+        })
     }
 }
 
