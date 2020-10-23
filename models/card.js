96fe056aa59d73d1a39b76a92cd00275436f5d31
Roman Pasternak <38011782+RomanPasternak@users.noreply.github.com>
2020-10-23 10:16:49 +0300

    add delete for basket

--- models/card.js
+++ models/card.js
@@ -39,6 +39,31 @@ class Card{
         })
     }
     
+    static async remove(id){
+        const card = await Card.fetch()
+
+        const idx = card.courses.findIndex(c => c.id === id)
+        const course = card.courses[idx]
+
+        if (course.count === 1){
+            card.courses = card.courses.filter(c => c.id !==id)
+        } else {
+            card.courses[idx].count--
+        }
+
+        card.price -= course.price
+
+        return new Promise((resolve, rejects) => {
+            fs.writeFile(p, JSON.stringify(card), err =>{
+                if(err){
+                    rejects(err)
+                } else {
+                    resolve(card)
+                }
+            })
+        })
+    }
+
     static async fetch(){
         return new Promise((resolve,rejects) =>{
             fs.readFile(p, 'utf-8', (err, content) =>{
